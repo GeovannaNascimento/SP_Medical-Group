f45ca3439ba986d170f628a5bfc98bce4c08c767
Caroline <125596928+C4rolcamargo@users.noreply.github.com>
2023-10-19 15:41:13 -0300

    Add files via upload

---
+++ sp medical/login1.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (email, cpf, senha) VALUES
+    ('livia@example.com', '123', 'senha_livia'),
+    ('carol@example.com', '123', 'senha_carol'),
+    ('isadora@example.com', '123', 'senha_isadora');
