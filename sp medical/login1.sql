8e15cfe5e0bcd34bbfc9aee1f34647579254c249
Caroline <125596928+C4rolcamargo@users.noreply.github.com>
2023-10-19 15:41:59 -0300

    Delete sp medical directory

--- sp medical/login1.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO usuarios (email, cpf, senha) VALUES
-    ('livia@example.com', '123', 'senha_livia'),
-    ('carol@example.com', '123', 'senha_carol'),
-    ('isadora@example.com', '123', 'senha_isadora');
